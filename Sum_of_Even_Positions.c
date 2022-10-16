a414139818aa8bacd581e50858d121074ab55223
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2022-10-16 17:00:11 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n,i;     cin>>n;     int arr[n];     //int a=0;     int b=0;     for(i=0;i<n;i++)     {     cin>>arr[i];     /*(if(i%2!=0)     {         a=a+arr[i];     }*/     if(i%2==0)     {         b=b+arr[i];     }     }     //cout<<a<<endl;     cout<<b<<endl; }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+    int arr[n];
+    //int a=0;
+    int b=0;
+    for(i=0;i<n;i++)
+    {
+    cin>>arr[i];
+    /*(if(i%2!=0)
+    {
+        a=a+arr[i];
+    }*/
+    if(i%2==0)
+    {
+        b=b+arr[i];
+    }
+    }
+    //cout<<a<<endl;
+    cout<<b<<endl;
+}
