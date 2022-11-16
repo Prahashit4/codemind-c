23fe6fce986d0f65fa367f79c5d2dbfcc5c65e91
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2022-11-16 23:49:47 +0530

    Create max b.w a & b.cpp

---
+++ max b.w a & b.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i,a,b,s,f=0;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    cin>>arr[i];
+    cin>>a>>b;
+    s=a;
+    for(i=0;i<n;i++){
+        if(a<=arr[i] && arr[i]<=b){
+            if(s<arr[i]){
+                s=arr[i];
+                f++;
+}}}
+    if(f!=0)
+    cout<<s;
+    else
+    cout<<-1;
+}
